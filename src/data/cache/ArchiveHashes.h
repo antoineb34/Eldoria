43d9e116ded6bd3dd4b9441eb8234c06acbf0503
antoineb34 <antoineballeux015@gmail.com>
2026-06-09 16:58:26 -0400

    Add data cache archive hash constants

---
+++ src/data/cache/ArchiveHashes.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+
+namespace rf::cache::archive {
+
+constexpr std::uint32_t ModelVersion = 0x386B7D48;
+constexpr std::uint32_t ModelCrc = 0xD2AE3F5D;
+constexpr std::uint32_t ModelIndex = 0xF2106D21;
+
+}
